f33793e216e843de6291843c19597f8ffdec8bb7
unknown <nellysherova@mail.ru>
2021-04-04 01:53:23 +0500

    Создала папку Controller, в ней контроллер QuotesController, добавила QuotesContext, добавила пакеты

---
+++ ApiHW/Controllers/QuotesController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiHW.Db;
+using Microsoft.EntityFrameworkCore;
+using ApiHW.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiHW.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        QuotesContext data { get; set; }
+        public QuotesController(QuotesContext context)
+        {
+            data = context;
+        }
+        [HttpPost]
+        public async Task<ActionResult<Quotes>> Create(Quotes newQuotes) //Create
+        {
+            if (newQuotes == null)
+            {
+                //ModelState.AddModelError("quoteError", "Добаляемые данные некоррентны!");
+                return BadRequest();
+            }
+            data.Quotes.Add(newQuotes);
+            await data.SaveChangesAsync();
+            return Ok("Данные добавлены!");
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Quotes>>> Read() // Select * 
+        {
+            return await data.Quotes.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Quotes>> Read(int id) //Select by id
+        {
+            Quotes quotes = await data.Quotes.FirstOrDefaultAsync(x => x.Id == id);
+            if (quotes == null)
+                return NotFound();
+            return new ObjectResult(quotes);
+        }
+        [HttpPut]
+        public async Task<ActionResult<Quotes>> Update(Quotes quotes)
+        {
+            if (quotes == null)
+            {
+                ModelState.AddModelError("quoteError", "Именяемые данные некоррентны!");
+                return BadRequest();
+            }
+            if (!data.Quotes.Any(x => x.Id == quotes.Id))
+            {
+                ModelState.AddModelError("quoteError", "Именяемые данные не найдены!");
+                return NotFound();
+            }
+            data.Update(quotes);
+            await data.SaveChangesAsync();
+            return Ok("Данные изменены!");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Quotes>> Delete(int id) //Delete
+        {
+            Quotes quotes = await data.Quotes.FirstOrDefaultAsync(x => x.Id == id);
+            if (quotes == null)
+            {
+                ModelState.AddModelError("quoteError", "Удаляемые данные не найдены!");
+                return NotFound();
+            }
+            data.Quotes.Remove(quotes);
+            await data.SaveChangesAsync();
+            return Ok("Данные удалены");
+        }
+
+        public void AutoDeteling(Quotes quotes)
+        {
+            DateTime quoteDate = quotes.InsertDate;
+            DateTime limitDate = DateTime.Now.AddMonths(1);
+            if (DateTime.Compare(quoteDate, limitDate) > 0)
+            {
+                data.Quotes.Remove(quotes);
+                data.SaveChanges();
+            }
+        }
+    }
+}
---
+++ ApiHW/Db/QuotesContext.cs
@@ -0,0 +1,60 @@
+﻿using ApiHW.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiHW.Db
+{
+    public class QuotesContext : DbContext
+    {
+        public DbSet<Quotes> Quotes { get; set; }
+        public QuotesContext (DbContextOptions<QuotesContext> options)
+            :base (options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Quotes>().HasData(
+                 new Quotes
+                 {
+                     Id = 1,
+                     Text = "Что разум человека может постигнуть и во что он может поверить, того он способен достичь",
+                     Author = "Наполеон Хилл",
+                     InsertDate = DateTime.Parse("15.10.2020")
+                 },
+                    new Quotes
+                    {
+                        Id = 2,
+                        Text = "Стремитесь не к успеху, а к ценностям, которые он дает",
+                        Author = "Альберт Эйнштейн",
+                        InsertDate = DateTime.Parse("02.11.1890")
+                    },
+                    new Quotes
+                    {
+                        Id = 3,
+                        Text = "Надо любить жизнь больше, чем смысл жизни.",
+                        Author = "Достоевский",
+                        InsertDate = DateTime.Parse("06.06.1890")
+                    },
+                    new Quotes
+                    {
+                        Id = 4,
+                        Text = "Сложнее всего начать действовать, все остальное зависит только от упорства",
+                        Author = "Амелия Эрхарт",
+                        InsertDate = DateTime.Parse("25.05.1800")
+                    },
+                    new Quotes
+                    {
+                        Id = 5,
+                        Text = "Начинать всегда стоит с того, что сеет сомнения.",
+                        Author = "Борис Стругацкий",
+                        InsertDate = DateTime.Parse("1.1.1100")
+                    }
+                    );
+        }
+    }
+}
