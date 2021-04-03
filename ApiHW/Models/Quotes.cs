70f5823b8339e972538dec380a82017e82642839
unknown <nellysherova@mail.ru>
2021-04-04 01:03:51 +0500

    Данные после сохранения

--- ApiHW/Models/Quotes.cs
+++ ApiHW/Models/Quotes.cs
@@ -7,5 +7,9 @@ namespace ApiHW.Models
 {
     public class Quotes
     {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Author { get; set; }
+        public DateTime InsertDate  { get; set; }
     }
 }
